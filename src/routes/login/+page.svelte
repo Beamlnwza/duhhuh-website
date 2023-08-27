1327c27633bfd8e9f5c4f0b3886938a17eeeac82
Beam <supawit04591@pcccr.ac.th>
2023-08-27 09:49:22 +0700

    init

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { googleAuthProvider } from '$lib/firebase';
+	import { signInWithPopup } from 'firebase/auth';
+	import { getFirebaseContext } from 'sveltefire';
+
+	const { auth } = getFirebaseContext();
+
+	async function login() {
+		// make sure auth is not undefined
+		if (!auth) {
+			throw new Error('Something went wrong try to refresh the page');
+		}
+
+		await signInWithPopup(auth, googleAuthProvider);
+	}
+</script>
+
+<button on:click={login}> Login with Google </button>
